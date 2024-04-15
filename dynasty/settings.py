829fb5fa5807be1b3ec5bf50e08b3ee0f182fe39
romanenko_vova <bonyshd@gmail.com>
2024-04-16 01:02:55 +0300

    a

--- dynasty/settings.py
+++ dynasty/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$3@tr-@pyu7dy9m-lky!6de=b4$)-wr+f_qu*bqz06)pr1v8p8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
